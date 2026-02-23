bf68a85a9674374e3b0e2cc9dd5789dd8961167a
Nilay Alwar <npalwar@tamu.edu>
2026-02-23 12:46:02 -0600

    Added onClick methods for main view buttons

---
+++ ManagerViewController.java
@@ -0,0 +1,25 @@
+import javafx.event.ActionEvent;
+
+public class ManagerViewController {
+
+    @FXML
+    void launchEmployeeView(ActionEvent event) {
+
+    }
+
+    @FXML
+    void launchInventoryView(ActionEvent event) {
+
+    }
+
+    @FXML
+    void launchPricesView(ActionEvent event) {
+
+    }
+
+    @FXML
+    void launchReportsView(ActionEvent event) {
+
+    }
+
+}
